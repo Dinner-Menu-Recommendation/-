418997d1ebcf70a6d4437bfca485b5955a79b781
Devinix00 <dpldpl123456@naver.com>
2024-06-07 22:41:40 +0900

    chore: css reset 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Reset } from "styled-reset";
 import styles from "./App.module.scss";
 
 function App() {
-  return <div className={styles.main}>ㅁㄴㄹㅇㅁㄴㄹㅁㄴㅇㄹㄴ</div>;
+  return (
+    <div className={styles.main}>
+      <Reset />
+    </div>
+  );
 }
 
 export default App;
