47a040d063079d8f83eee9663b71ce875ad904db
Devinix00 <dpldpl123456@naver.com>
2024-06-07 23:28:33 +0900

    feat: 통신 테스트

--- src/api/apiInstance.ts
+++ src/api/apiInstance.ts
@@ -1,12 +1,14 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 const apiInstance: AxiosInstance = axios.create({
-  baseURL: "",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-apiInstance.interceptors.response.use(
-  (response: AxiosResponse) => {
-    return response;
+apiInstance.interceptors.request.use(
+  (config) => {
+    config.params = config.params || {};
+    config.params["apiKey"] = import.meta.env.VITE_API_KEY;
+    return config;
   },
   (error: AxiosError) => {
     console.error(error);
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import styles from "./Home.module.scss";
+import apiInstance from "../../api/apiInstance";
 
 function Home() {
-  console.log(import.meta.env.VITE_API_KEY);
+  const [data, setData] = useState();
+
+  const fetch = async () => {
+    try {
+      const res = await apiInstance.get("/recipes/complexSearch");
+      setData(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  console.log(data);
 
   return <div className={styles.main}>Home</div>;
 }
