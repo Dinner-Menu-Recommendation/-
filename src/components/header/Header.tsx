4683b1f0308209c3c9209e84662d54d8630de517
Devinix <121074744+Devinix00@users.noreply.github.com>
2024-07-01 18:11:21 +0900

    feat: 결과 페이지 헤더 퍼블리싱

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { ChevronLeft } from "../../assets/svg";
 import styles from "./Header.module.scss";
+import { ArrowLeft } from "../../assets/svg";
 
 function Header() {
   const navigate = useNavigate();
@@ -9,10 +9,16 @@ function Header() {
     <header className={styles.header}>
       <img
         onClick={() => navigate(-1)}
-        src={ChevronLeft}
+        src={ArrowLeft}
         alt="뒤로가기"
         className={styles.back_button}
       />
+
+      <div className={styles.question_numbers}>
+        <p>1</p>
+        <p>/</p>
+        <p>10</p>
+      </div>
     </header>
   );
 }
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --black: #1B1B1C;
+    --gray: #999987;
+  }
+
   html {
     font-size: 62.5%; 
   }
